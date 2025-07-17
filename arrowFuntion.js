9ddebfac94b2b1a18bcf6f7d37d969609f12fc0f
chris17371 <christhianlira8@gmail.com>
2025-07-17 17:04:45 -0400

    verArrow

---
+++ arrowFuntion.js
@@ -0,0 +1,16 @@
+const aprendiendo= function() {
+    console.log("Aprendiendo JavaScript");
+}
+const aprendiendo2= () => {
+    console.log("Aprendiendo JavaScript 2");
+}
+
+//una linea no necesita llaves
+const aprendiendo3= () => console.log("Aprendiendo JavaScript 3");
+
+//retornar valor
+const aprendiendo4= () =>"Aprendiendo JavaScript 4";
+
+console.log(aprendiendo());
+console.log(aprendiendo4());
+console.log(aprendiendo3());
